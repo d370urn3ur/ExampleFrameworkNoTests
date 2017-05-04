e4e52ed7340fb414d4a3e8082f3522d96e436ed1
= <jpierce@myluckyday.fr>
2017-05-04 11:20:23 +0200

    Initial Commit

---
+++ ExampleFrameworkNoTests/ExampleFrameworkNoTests.h
@@ -0,0 +1,19 @@
+//
+//  ExampleFrameworkNoTests.h
+//  ExampleFrameworkNoTests
+//
+//  Created by MacBook on 5/4/17.
+//  Copyright © 2017 MyLuckyDay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleFrameworkNoTests.
+FOUNDATION_EXPORT double ExampleFrameworkNoTestsVersionNumber;
+
+//! Project version string for ExampleFrameworkNoTests.
+FOUNDATION_EXPORT const unsigned char ExampleFrameworkNoTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleFrameworkNoTests/PublicHeader.h>
+
+

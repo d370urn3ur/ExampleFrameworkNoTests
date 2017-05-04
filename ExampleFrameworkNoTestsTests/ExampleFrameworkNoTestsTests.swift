0fbaa51c158a2a5a822bd086637757d8e008c13e
= <jpierce@myluckyday.fr>
2017-05-04 11:41:37 +0200

    inital commit

--- ExampleFrameworkNoTestsTests/ExampleFrameworkNoTestsTests.swift
+++ ExampleFrameworkNoTestsTests/ExampleFrameworkNoTestsTests.swift
@@ -24,6 +24,8 @@ class ExampleFrameworkNoTestsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        XCTAssertTrue(false, "failing test to test tests")
     }
     
     func testPerformanceExample() {
